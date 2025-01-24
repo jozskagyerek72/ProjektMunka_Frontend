4dab28d7b46ac5ce060ccfd06421e5421688b880
jozskagyerek72 <gillichbalint@gmail.com>
2025-01-24 11:51:58 +0100

    implement getworkers

---
+++ src/utils/crudUtil.js
@@ -0,0 +1,16 @@
+import { collection, getDocs, onSnapshot, orderBy, query } from "firebase/firestore"
+import { db } from "./firebaseApp"
+
+export const readWorkers = async ( setWorkers ) =>
+{
+        /*const querySnapshot = await getDocs(collection(db, "workers"))
+        let workers = []
+        querySnapshot.forEach((doc)=>  { workers.push({...doc.data(), id:doc.id}) })
+        return workers*/
+        
+        const collectionRef = collection(db, "workers")
+        const q = query(collectionRef, orderBy("name", "asc"))
+        const unsubscribe = onSnapshot(q , (snapshot) => { setWorkers(snapshot.docs.map( (doc) => ({...doc.data(),id:doc.id}) )) } )
+        return unsubscribe
+}
+    
