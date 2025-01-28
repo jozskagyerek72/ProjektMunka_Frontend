5ca17e6ecf6ed0ade15212b86f412e5fb68900c5
MagyarBene <magyar.bene@gmail.com>
2025-01-28 20:44:16 +0100

    DaisyUI

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,23 @@
-import React from 'react'
-import { useState } from 'react'
-import { readWorkers } from '../utils/crudUtil'
+import {React} from 'react'
+import { Card } from '../components/card'
+//import { useState } from 'react'
+//import { readWorkers } from '../utils/crudUtil'
 
 export const Home = () => {
-
-    const [workers, setWorkers] = useState([])
-    readWorkers(setWorkers)
+    //const [workers, setWorkers] = useState([])
+    //readWorkers(setWorkers)
     
     return (
+     <div className='page bg-gray-950 flex'>
+      <Card/> 
+     </div>
+     /* 
     <div>
       {workers.map(worker=>
         <p key={worker.id}> {worker.id} : {worker.name} : {worker.field} : {worker.hourlypay}</p>
       )}
     </div>
+    */
   )
 }
 
--- src/utils/firebaseApp.js
+++ src/utils/firebaseApp.js
@@ -1,11 +1,11 @@
-import { initializeApp } from "firebase/app";
+/*import { initializeApp } from "firebase/app";
 import { firebaseConfig } from "./firebaseConfig.js";
-import { collection, doc, getDocs, getFirestore } from "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const app = initializeApp(firebaseConfig);
 
-//referenciak:
+
 export const db = getFirestore(app)
 export const auth = getAuth(app)
-
+*/
