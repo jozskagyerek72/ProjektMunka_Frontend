83e54ef23d8e1cfc66aee4179c90cde994342004
MagyarBene <magyar.bene@gmail.com>
2025-01-28 21:18:16 +0100

    test

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -10,7 +10,7 @@ export const Card = () => {
                     <h2 className="card-title">Shoes!</h2>
                     <p>If a dog chews shoes whose shoes does he choose?</p>
                     <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Buy Now</button>
+                        <button className="btn btn-primary">Details</button>
                     </div>
                     </div>
                 </div>
