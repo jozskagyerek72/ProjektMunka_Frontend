e495b00ac162fe72a4db04348d1fd15e50049b4c
mdavidstefan <stef.david.marton@gmail.com>
2025-02-07 12:23:48 +0100

    added updateuser to usercontext

--- src/components/Toast.jsx
+++ src/components/Toast.jsx
@@ -5,7 +5,7 @@ import { useEffect } from "react"
 import { toast } from "sonner"
 import { Toaster } from "sonner"
 
-export const Toast = ({ signin, err, signup, signout, resetpassword }) => {
+export const Toast = ({ signin, err, signup, signout, resetpassword, update }) => {
     const { setMsg } = useContext(UserContext)
     const navigate = useNavigate()
 
@@ -14,13 +14,16 @@ export const Toast = ({ signin, err, signup, signout, resetpassword }) => {
             toast.error(err, { position: 'top-left' })
         } else if (signin || signup) {
             toast.success(signin || signup, { position: 'top-center' })
+            setTimeout(() => navigate('/'), 1000)
         } else if (resetpassword || signout) {
             toast.success(resetpassword || signout, { position: 'top-center' })
             setTimeout(() => navigate('/authentication/signin'), 1000)
+        } else if (update) {
+            toast.success(update, {position: 'top-center'})
         }
 
         setMsg({})
-    }, [signin, err, signup, signout, resetpassword])
+    }, [signin, err, signup, signout, resetpassword, update])
 
     return (
         <div>
