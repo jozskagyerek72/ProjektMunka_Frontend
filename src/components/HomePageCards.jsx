c84169de06308dce26e2bbc9cbb7af6fe3f436e1
MagyarBene <magyar.bene@gmail.com>
2025-03-13 12:44:40 +0100

    applyWIP/header

--- src/components/HomePageCards.jsx
+++ src/components/HomePageCards.jsx
@@ -42,7 +42,7 @@ export const HomePageCards = ({setRole}) => {
           <h2 className="card-title text-4xl">Apply</h2>
           <p className="cardtext text-xl">Apply for a job.</p>
           <div className="card-actions justify-end">
-            <button className="btn btn-primary" onClick={()=>navigate("/authentication/signup")}>Apply</button>
+            <button className="btn btn-primary" onClick={()=>navigate("/apply")}>Apply</button>
           </div>
         </div>
       </div>
