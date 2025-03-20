9b30fa86fb549c7724baddcba1a2a849cbe61a53
jozskagyerek72 <gillichbalint@gmail.com>
2025-03-20 15:25:57 +0100

    commit

--- src/pages/Analytics.jsx
+++ src/pages/Analytics.jsx
@@ -13,8 +13,7 @@ export const Analytics = () => {
   const [workedHours, setWorkedHours] = useState(null);
   const [payment, setPayment] = useState(null);
 
-  const [workedHours, setWorkedHours] = useState(null)
-  const [payment, setPayment] = useState(null)
+  
   
 
   useEffect(() => {
