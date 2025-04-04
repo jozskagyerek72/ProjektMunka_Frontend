5bc02f7b37f51768c747e263f49ff1b52981a0b6
MagyarBene <magyar.bene@gmail.com>
2025-04-04 11:36:30 +0200

    analytics table

--- src/pages/WorkerDetails.jsx
+++ src/pages/WorkerDetails.jsx
@@ -106,9 +106,9 @@ export const WorkerDetails = () => {
       )}
 
       <div className="text-center mt-5">
-        <h1 className="font-bold text-3xl">Recent shifts</h1>
+        <h1 className="font-bold text-3xl text-white">Recent shifts</h1>
       </div>
-      <table className="table-fixed border-collapse mt-3 bg-gray-700 rounded-xl text-center shadow-md shadow-gray-700">
+      <table className="table-fixed border-collapse mt-3 bg-gray-700 rounded-xl text-center shadow-md text-white shadow-gray-700">
         <thead>
           <th className="border border-b-gray-600 border-x-0 border-t-0 text-sm md:text-xl py-2 px-2">
             Shift start
