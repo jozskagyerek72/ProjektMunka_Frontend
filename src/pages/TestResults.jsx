8962991a6974a40d08fbf2d66ec711ce47036e53
mdavidstefan <stefan.d.marton@gmail.com>
2025-04-12 18:34:55 +0200

    reduced size of homepagecards by creating cards object array, final commit

--- src/pages/TestResults.jsx
+++ src/pages/TestResults.jsx
@@ -3,13 +3,13 @@ import testResults from "../../results.json";
 
 export const TestResults = () => {
   return (
-    <div className="min-h-dvh bg-gray-950 flex flex-col justify-center items-center pb-10">
+    <div className="min-h-dvh bg-gray-950 flex flex-col justify-center items-center pb-10 px-5">
       <div className="text-center justify-center flex flex-wrap mt-10">
         <h1 className="text-3xl wlh12 font-bold m-0 md:m-17">
           Tests & results
         </h1>
       </div>
-      <div className="flex flex-col justify-evenly items-center bg-gray-800 rounded-lg w-auto p-4 gap-5 mx-4">
+      <div className="flex flex-col justify-evenly items-center bg-gray-800 rounded-lg w-auto p-4 gap-5">
         <h1 className="text-3xl font-bold text-white">Tests - summary</h1>
         <div className="flex flex-col md:flex-row items-center justify-evenly w-md md:w-full gap-5">
           <div className="rounded-xl p-4 bg-gray-600 border border-transparent hover:border-gray-400 text-primary-content transition-all duration-300 hover:scale-[1.02] hover:shadow-lg hover:shadow-gray-800/50 flex flex-col justify-center items-center w-full h-full">
